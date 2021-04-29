0d88b3794fa2e31cc5979188c07f8d48c0ec0290
yusame0308 <yu.starts.yu@gmail.com>
2021-04-29 23:51:09 +0900

    first commit

---
+++ lib/general_providers.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final firebaseAuthProvider =
+    Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
+
+final firebaseFirestoreProvider =
+    Provider<FirebaseFirestore>((ref) => FirebaseFirestore.instance);
